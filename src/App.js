94e27f19971ec3a566c618f075fceceed9588a0a
João Victor <joao5victor5ss@gmail.com>
2022-06-23 19:16:06 -0300

    iniciando firebase

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Normalize } from 'styled-normalize';
 
 import Routes from './routes';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import Theme from './styles/Theme';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Theme>
-      <Routes />
+      <BrowserRouter/>
+      <Routes/>
       <GlobalStyle />
       <Normalize />
     </Theme>
