cfc6b6da48af27c70e29dcb756e663f18f0ec914
João Victor <joao5victor5ss@gmail.com>
2022-06-24 15:49:16 -0300

    .

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import Routes from './routes';
 import Theme from './styles/Theme';
 import GlobalStyle from './styles/global';
 
-// contexto de autenticação
-
-import { AuthProvider } from './context/AuthContext';
-
-
 
 function App() {
   return (
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// contexto de autenticação ------------------------------------------------------
+import { onAuthStateChanged } from 'firebase/auth';
+import { AuthProvider } from '../../context/AuthContext';
+// hook para verificar se o usuário está logado
 
+import { useAuthentication } from '../../hooks/useAuthentication';
+// ---------------------------------------------------------------------------------------
 import Login from './Login';
 import MenuComponent from '../../components/MenuComponent';
 
+
+  
 function Main() {
+
+  // isso aqui joga pra dentro do App.js
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+// ----------------------------
   return (
     <div>
     <Login/>
