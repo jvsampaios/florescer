0dd66ee8efc6916707ecd314b8147e00b8a98edf
João Victor <joao5victor5ss@gmail.com>
2022-07-07 19:03:39 -0300

    .

---
+++ src/pages/Planta/index.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import {
+    Layout,
+    Typography,
+    Row,
+    Col,
+    Button,
+    Image,
+    Card,
+    Collapse,
+} from 'antd';
+import { useHistory } from 'react-router-dom';
+import './styles.css'
+import { ArrowLeftOutlined, DownOutlined } from '@ant-design/icons';
+import planta from "../../../assets/planta.png";
+import calendar from "../../../assets/calendar_icon.svg";
+import adubagem from "../../../assets/adubagem_icon.svg";
+import vasos from "../../../assets/vasos_icon.svg";
+import clima from "../../../assets/clima_icon.svg";
+import "antd/dist/antd.css";
+import MenuComponent from '../../../components/MenuComponent';
+
+const { Title, Text } = Typography;
+const { Content } = Layout;
+const { Panel } = Collapse;
+const genExtra = () => (
+    <DownOutlined id='icon-collapse'
+        onClick={() => {
+            document.getElementById("icon-collapse").style.transform = "rotate(180deg);";
+        }}
+    />
+
+);
+
+const Planta = () => {
+    const history = useHistory();
+    return (
+        <Layout className="layout" >
+            <Row className='container_item' style={{ justifyContent: 'flex-start', marginBottom: "0" }}>
+                <Col>
+                    <Button type='link' style={{ color: '#6D7970' }} onClick={history.goBack}>
+                        <ArrowLeftOutlined style={{ fontSize: '26px', padding: 0 }} />
+                    </Button>
+                </Col>
+            </Row>
+            <Row className='container_item'>
+                <Title className='titulo' style={{ marginTop: "0" }}>Cacto Juliete</Title>
+            </Row>
+            <Content className="site-layout-content" style={{ display: 'block', padding: 0 }} >
+                <Row className='container_item' style={{ marginBottom: '5rem' }}>
+                    <Col>
+                        <Card className='card-planta-style'>
+                            <Text className='text-planta'>Texto aqui</Text>
+                        </Card>
+                        <Image
+                            src={planta}
+                            preview={false}
+                            className='planta'
+                        />
+                    </Col>
+                </Row>
+                <Row className='container_item' style={{ padding: '0 15px', textAlign: 'justify' }}>
+                    <Col>
+                        <Text className='sobre-planta'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posure metus mi leo donec imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posure metus mi leo donec imperdiet.</Text>
+                    </Col>
+                </Row>
+                <Row className='container_item' >
+                    <Collapse className='sobre-planta'
+                        bordered={false}
+                        expandIcon={() => <Image className='icon-collapse' src={calendar} />}
+                        style={{
+                            width: '100%',
+                            backgroundColor: 'transparent'
+                        }}>
+                        <Panel extra={genExtra()} header="Cronograma" key="1" >
+                            <Text className='sobre-planta'>Cronograma</Text>
+                        </Panel>
+                    </Collapse>
+                    <Collapse
+                        className='sobre-planta'
+                        bordered={false}
+                        expandIcon={() => <Image className='icon-collapse' src={adubagem} />}
+                        style={{
+                            width: '100%',
+                            backgroundColor: 'transparent'
+                        }}>
+                        <Panel extra={genExtra()} header="Adubagem" key="1" >
+                            <Text className='sobre-planta'>Adubagem</Text>
+                        </Panel>
+                    </Collapse>
+                    <Collapse
+                        className='sobre-planta'
+                        bordered={false}
+                        expandIcon={() => <Image className='icon-collapse' src={vasos} />}
+                        style={{
+                            width: '100%',
+                            backgroundColor: 'transparent'
+                        }}>
+                        <Panel extra={genExtra()} header="Vasos" key="1" >
+                            <Text className='sobre-planta'>Vasos</Text>
+                        </Panel>
+                    </Collapse>
+                    <Collapse
+                        className='sobre-planta'
+                        bordered={false}
+                        expandIcon={() => <Image className='icon-collapse' src={clima} />}
+                        style={{
+                            width: '100%',
+                            backgroundColor: 'transparent'
+                        }}>
+                        <Panel extra={genExtra()} header="Clima Ideal" key="1" >
+                            <Text className='sobre-planta'>Clima Ideal</Text>
+                        </Panel>
+                    </Collapse>
+                </Row>
+
+            </Content>
+            <MenuComponent />
+        </Layout >
+    );
+};
+
+
+export default Planta;
